5a102e15c5f3cfc67fc8f0e0b816fe75ac9fe922
CristhianCC <cristhianmcm12@gmail.com>
2021-05-04 14:16:23 +0200

    Variable in component test

--- 2-React_First_Approach/1-counter-app/src/FirstApp.js
+++ 2-React_First_Approach/1-counter-app/src/FirstApp.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 // Functional Component
 const FirstApp = () => {
+    const greeting = "Hello everyone!";
+
     return (
         <>
-            <h1>Hello World!</h1>
+            <h1> { greeting } </h1>
             <p>My first application</p>
         </>
     );
