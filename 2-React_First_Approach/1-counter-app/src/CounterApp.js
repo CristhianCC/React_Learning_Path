5105c4a72fc755725a716da67319aa16c706db95
CristhianCC <cristhianmcm12@gmail.com>
2021-05-05 13:14:19 +0200

    Some css and event handlers in React

--- 2-React_First_Approach/1-counter-app/src/CounterApp.js
+++ 2-React_First_Approach/1-counter-app/src/CounterApp.js
@@ -3,10 +3,18 @@ import PropTypes from 'prop-types';
 
 
 const CounterApp = ({ number }) => {
+
+    //handleAdd
+    const handleAdd = (event) => {
+        console.log(event);
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <p> { number } </p>
+
+            <button onClick={ handleAdd }> ++ </button>
         </>
     );
 };
