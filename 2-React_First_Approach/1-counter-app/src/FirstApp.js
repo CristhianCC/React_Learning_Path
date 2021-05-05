bf9f516e39f9d135810cf8e7278e9b70a060eb54
CristhianCC <cristhianmcm12@gmail.com>
2021-05-05 12:04:41 +0200

    Introduction of PropTypes

--- 2-React_First_Approach/1-counter-app/src/FirstApp.js
+++ 2-React_First_Approach/1-counter-app/src/FirstApp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // Functional Component
 const FirstApp = ({greeting = 'Hello, Cristhian!'}) => {
@@ -8,6 +9,10 @@ const FirstApp = ({greeting = 'Hello, Cristhian!'}) => {
             <p>My first application</p>
         </>
     );
+}
+
+FirstApp.propTypes = {
+    greeting: PropTypes.string.isRequired
 }
 
 export default FirstApp;
