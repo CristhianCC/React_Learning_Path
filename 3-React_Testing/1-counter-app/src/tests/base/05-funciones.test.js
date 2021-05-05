ebb230cedf5b7df2486848e936d68970a6daabfc
CristhianCC <cristhianmcm12@gmail.com>
2021-05-05 16:43:11 +0200

    Equality test refactor

--- 3-React_Testing/1-counter-app/src/tests/base/05-funciones.test.js
+++ 3-React_Testing/1-counter-app/src/tests/base/05-funciones.test.js
@@ -1,16 +1,25 @@
-import { getUser } from '../../base/05-funciones';
+import { getUser, getUsuarioActivo } from '../../base/05-funciones';
 
 describe( 'Tests in 05-funciones.test.js', () => {
 
     test('getUser should return an object', () => {
-
         const userTest = {
             uid: 'ABC123',
             username: 'El_Papi1502'
         };
-
         const user = getUser();
-
         expect( userTest ).toEqual( user );
+
+    });
+
+    test('getUsuarioActivo should return an object', () => {
+
+        const name = 'Cristhian';
+        const usuarioActivoTest = getUsuarioActivo('Cristhian');
+        expect( usuarioActivoTest ).toEqual({
+            uid: 'ABC567',
+            username: name
+        } );
+
     });
 });
