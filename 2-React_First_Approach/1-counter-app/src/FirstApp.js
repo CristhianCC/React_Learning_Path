e67f77db68495569f2fa932f66a43c94377d0049
CristhianCC <cristhianmcm12@gmail.com>
2021-05-05 12:08:03 +0200

    Default props introduction

--- 2-React_First_Approach/1-counter-app/src/FirstApp.js
+++ 2-React_First_Approach/1-counter-app/src/FirstApp.js
@@ -2,17 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Functional Component
-const FirstApp = ({greeting}) => {
+const FirstApp = ({ greeting, subtitle }) => {
     return (
         <>
-            <h1> {greeting} </h1>
-            <p>My first application</p>
+            <h1> { greeting } </h1>
+            <p> { subtitle } </p>
         </>
     );
 }
 
 FirstApp.propTypes = {
     greeting: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+    subtitle: 'I am a subtitle'
 }
 
 export default FirstApp;
