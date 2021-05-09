ef84d499eaa3c4e0ef549ed0873637c181db8237
CristhianCC <cristhianmcm12@gmail.com>
2021-05-09 12:12:40 +0200

    Creation of tests with Enzyme

--- 3-React_Testing/1-counter-app/src/tests/FirstApp.test.js
+++ 3-React_Testing/1-counter-app/src/tests/FirstApp.test.js
@@ -1,11 +1,20 @@
 import FirstApp from '../FirstApp';
 import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 
 describe('Testing FirstApp', () => {
-    test('should return Hello, Im Cristhian', () => {
-      const greeting = 'Hello, Im Cristhian';
-      const { getByText } = render( <FirstApp greeting={greeting} />);
+    // test('should return Hello, Im Cristhian', () => {
+    //   const greeting = 'Hello, Im Cristhian';
+    //   const { getByText } = render( <FirstApp greeting={greeting} />);
+    //
+    //   expect(getByText(greeting)).toBeInTheDocument();
+    // });
 
-      expect(getByText(greeting)).toBeInTheDocument();
+    test('should return <FirstApp /> correctly', () => {
+        const greeting = 'Hello, Im Cristhian';
+        const wrapper = shallow(<FirstApp greeting={ greeting } /> );
+
+        expect(wrapper).toMatchSnapshot();
     });
+
 });
